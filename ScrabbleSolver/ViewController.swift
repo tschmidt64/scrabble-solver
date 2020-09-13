29d98905248ed84d0ebf2a205963c8ee8cae9e60
Taylor Schmidt <tschmidt@skygrid.com>
2020-09-13 15:04:30 -0500

    Initial Commit

---
+++ ScrabbleSolver/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScrabbleSolver
+//
+//  Created by Taylor Schmidt on 9/13/20.
+//  Copyright © 2020 Taylor Schmidt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
